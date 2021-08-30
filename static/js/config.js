57af7eb0355236a5e6cd0b2eb0e937813f969ee8
unknown <saiyidmkazmi@gmail.com>
2021-08-30 18:37:19 -0500

    adding

---
+++ static/js/config.js
@@ -0,0 +1,3 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoic2hhcnAyNDU0IiwiYSI6ImNrcWxqMmN1bjAzNDkyb3BlNzNxMDZnOHAifQ.rCALevh4qPQlfkp_eauprA";
+//Dont need it because i used openstreetmap which doesnt require an API or Access Token
